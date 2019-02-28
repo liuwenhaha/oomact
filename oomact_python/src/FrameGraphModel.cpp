92667447badd637204e1f0a280a65c69297cb8d7
Yannick Huber <huberya@student.ethz.ch>
2019-02-28 19:01:19 +0100

    found way to do FrameGraphModel, need to do base

--- oomact_python/src/FrameGraphModel.cpp
+++ oomact_python/src/FrameGraphModel.cpp
@@ -1,17 +1,35 @@
 #include <numpy_eigen/boost_python_headers.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/python.hpp>
+#include <boost/python/suite/indexing/vector_indexing_suite.hpp>
 
 #include <aslam/calibration/model/FrameGraphModel.h>
 
 
 using namespace boost::python;
 using namespace aslam::calibration;
+
+/*void exportModel()
+{
+    //TODO
+    //void addModulesAndInit(Modules_ & ... modules)
+}*/
+
 void exportFrameGraphModel()
 {
+    typedef std::vector<const Frame*> FramePointers;
+
+    class_<FramePointers>("FramePointerVector")
+    .def(boost::python::vector_indexing_suite<FramePointers>() )
+    .def("__iter__", boost::python::iterator<FramePointers>())
+    ;
+
     //FrameGraphModel(ValueStoreRef config, std::shared_ptr<ConfigPathResolver> configPathResolver = nullptr, const std::vector<const Frame *> frames = {})
-    //class_<FrameGraphModel, boost::shared_ptr<FrameGraphModel>>("FrameGraphModel", init<ValueStoreRef, std::shared_ptr<ConfigPathResolver>, std::vector<Frame *>()) //how to define default values
-    //.def("getAtTime", &FrameGraphModel::getAtTime)
+    class_<FrameGraphModel, boost::shared_ptr<FrameGraphModel>, boost::noncopyable>("FrameGraphModel", no_init)
+    .def(init<ValueStoreRef, std::shared_ptr<ConfigPathResolver>, const FramePointers>()) //how to define default values
+    //ModelAtTime getAtTime(Timestamp timestamp, int maximalDerivativeOrder, const ModelSimplification & simplification) const override
+    /*.def("getAtTimeTimestamp", static_cast(<ModelAtTime(FrameGraphModel::*)(Timestamp, int, const ModelSimplification&)const>(&FrameGraphModel::getAtTime))*/
     //void registerModule(Module & m) override
-    //.def("registerModule", &FrameGraphModel::registerModule)
+    /*.def("registerModule", &FrameGraphModel::registerModule)*/
+    ;
 }
